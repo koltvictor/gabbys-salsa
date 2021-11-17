e52ce32097bb15ba6289857b83135483a25d894f
koltvictor <kolts.email@gmail.com>
2021-11-17 17:43:25 -0500

    styling

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ export default function About ({currentUser}) {
         <div className="about">
             <h1 className="aboutHead">About Gabby</h1>
             <p className="aboutBody">
-            <strong>Hi, {currentUser.name}</strong>!  And welcome to Gabby's Salsa!  Hold on to your hat, because Gabby uses the freshest, highest quality ingredients to create the best salsas you can imagine.  She's kind of remarkable.<br/><br/>
+            <strong>Hi, {currentUser.name}</strong>!  And welcome to Gabby's Salsa!  Hold on to your hat, because Gabby uses the freshest, highest quality ingredients to create the best salsas you can imagine.  They're all handmade, small batches.  She's kind of remarkable.<br/><br/>
                 La Mexicana is a maker of salsas made with farm-fresh ingredients. We source our ingredients from farms that grow the freshest vegetables year ’round — firm sun-ripened tomatoes, crisp onion, zesty jalapeño, and fresh cilantro. 
                 Our farms are close to our kitchens, so when the crated veggies arrive, we work quickly to capture all the goodness, texture, and flavor. We chop, blend, season, and pack in 48 hours. This gives you a product that­’s perfect with chips or as an accompaniment to your favorite recipes.
 <br/><br/>
