cbc5194ed2a1f33dcd7ba20bb82c6210e6f430b0
koltvictor <kolts.email@gmail.com>
2021-11-27 10:57:55 -0800

    added flip cards to product page

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -4,12 +4,19 @@ export default function ProductCard({product, handleAddToCart}) {
 
 
     return(
-        <div className="productCard">
-                <img className="productImg" src={product.image} alt={product.name} /><br/><br/>
-                <div className="productName">{product.name}<br/></div><br/>
-                <div className="productDescription">{product.description}<br/></div><br/>
-                <div className="productPrice">${product.price}<br/></div><br/>
-                <button className="addToCartButton" onClick={() => {handleAddToCart(product)}}>Add to Cart</button>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <img className= "productImg" src={product.image} alt={product.name} />
+                    <div classname="productName">{product.name}<br/></div><br/>
+                </div>
+                <div className="flip-card-back">
+                    <br/>
+                    <div >{product.description}<br/></div><br/>
+                    <div >${product.price}<br/></div><br/>
+                    <button className="addToCartButton" onClick={() => {handleAddToCart(product)}}>Add to Cart</button>
+                </div>
+            </div>
         </div>
     )
 }
