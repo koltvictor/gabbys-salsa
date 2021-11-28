3ffd372b6fcd82b4aed22b9955d6b0f4499d2caf
koltvictor <kolts.email@gmail.com>
2021-11-28 11:15:30 -0800

    fixed it by replacing index.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Auth from './Auth';
 import UnAuth from './UnAuth';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import {history} from 'history'
 
 
 
